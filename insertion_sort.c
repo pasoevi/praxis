4f1b66285c489fbc5f697b9e3a5770b129b535b0
Sergi Pasoev <s.pasoev@gmail.com>
2017-01-15 19:18:06 +0400

    initial commit

---
+++ insertion_sort.c
@@ -0,0 +1,3 @@
+int insertion_sort(int arr[]) {
+    // int arr[] = [1, 2, 5, 3, 9];
+};
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    robotour();
+    getchar();
+    return 0;
+}
