f64ff61610f1641fd7c6236343cbf1813addd9e7
Sergi Pasoev <s.pasoev@gmail.com>
2017-01-26 22:59:25 +0400

    code examples

--- clj/codewars/src/codewars/core.clj
+++ clj/codewars/src/codewars/core.clj
@@ -1,9 +1,10 @@
 (ns codewars.core
   (:require [codewars.persistence :refer :all]
             [codewars.avg :refer :all]
+            [codewars.weights :refer :all]
             [codewars.cubes :refer :all])
   (:gen-class))
-  
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -12,6 +13,6 @@
   (println "Testing persistence")
   (println "Testing cubes")
   ;; (println (get-nth-cube (read-string (first args))))
-  (println (find-nb (read-string (first args))))
-  (println (persistence (read-string (first args)))))
+  ;;(println (find-nb (read-string (first args))))
+  (println (order-weight "2000 10003 1234000 44444444 9999 11 11 22 123")))
 
