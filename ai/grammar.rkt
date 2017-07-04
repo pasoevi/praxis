6f0b8b1e3a66c4bfa04e1ce68108be39a3061256
Sergi Pasoev <s.pasoev@gmail.com>
2017-07-04 09:58:31 +0400

    racket examples

--- ai/grammar.rkt
+++ ai/grammar.rkt
@@ -1,34 +1,46 @@
 #lang racket
 
-(define articles '("the" "a" "an"))
-(define nouns '("dog" "cat" "man" "woman"))
-(define verbs '("banks" "reads" "jumps"))
+;; Vocabulary
+(define nouns '(antilope elephant dog cat man woman))
+(define verbs '(barks reads jumps))
 
+;; List functions
 (define (one-of set)
   "Pick one element of set, and make a list of it"
-  (random-elt set))
+  (list (random-elt set)))
 
 (define (random-elt choices)
   "Choose an alement from a list at random"
-  (list-ref choices (random (length articles))))
+  (list-ref choices (random (length choices))))
 
 (define (contains ls elem)
   "Check if ls contains element elem"
   (> (length (filter (λ (x) (= x elem)) ls)) 0))
 
+;; String functions
+(define (starts-with str c)
+  "Check whether c is the first character of str. Case insensitive"
+  (if (char-ci=? (string-ref str 0) c) #t #f))
+
+;; Grammar functions
+(define (vowel? c)
+  (member c (list #\a #\e #\i #\o #\u)))
+
 (define (article noun)
   "Pick a correct article given a noun"
-  (display "Ok"))
-  
+  (if (vowel? (string-ref (symbol->string (first noun)) 0)) '(an) '(a)))
+
+(define (noun) (one-of nouns))
+(define (verb) (one-of verbs))
 
 (define (noun-phase)
-  (let ([noun (list (one-of nouns))])
-    (cons (one-of articles) noun)))
+  (let ([some-noun (noun)])
+    (append (article some-noun) some-noun)))
 
-  (define (verb-phase)
-    (list(one-of verbs)))
+(define (verb-phase)
+  (append (verb) (noun-phase)))
 
-  (define (sentence)
-    (append (noun-phase) (verb-phase)))
+(define (sentence)
+  (append (noun-phase) (verb-phase)))
 
-  (sentence)
+(sentence)
