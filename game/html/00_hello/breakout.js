23e9716b7061888d688436c01f3f842bf75c2e1c
Sergi Pasoev <s.pasoev@gmail.com>
2017-08-21 01:22:58 +0400

    paint example (canvas)

--- game/html/00_hello/breakout.js
+++ game/html/00_hello/breakout.js
@@ -1,29 +1,169 @@
 window.onload = function() {
     //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-    //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
+    //  Although it will work fine with this tutorial, it"s almost certainly not the most current version.
     //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
+
+    let paddle;
+    let ball;
+    let bricks;
+
+    let ballOnPaddle = true;
+
+    let lives = 3;
+    let score = 0;
+
+    let scoreText;
+    let livesText;
+    let introText;
+
     const game = new Phaser.Game(
       800, 600,
-       Phaser.AUTO, "game", { preload: preload, create: create });
-
-    const bricks = [];
-    const ROWS = 5, COLS = 20, OFFSET = 10, BRICK_W = 50, BRICK_H = 20;
-
-    for (let i = 0; i < ROWS; i += 1) {
-      for (let j = 0; j < COLS; j += 1) {
-        bricks.push({
-          x: i * (BRICK_H + OFFSET),
-          y: j * (BRICK_W + OFFSET)
-        });
-      }
-    }
+       Phaser.AUTO, "game", { preload: preload, create: create, update: update });
 
     function preload () {
-        game.load.image('logo', 'paddle.png');
+        game.load.image("paddle", "paddle.png");
+        game.load.image("brick", "brick.png");
+        game.load.image("ball", "ball.png");
     }
 
     function create () {
-        const logo = game.add.sprite(game.world.centerX, 600, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        // Setup game and physics
+        game.stage.backgroundColor = '#111';
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.checkCollision.down = false;
+
+        // Create paddle
+        paddle = game.add.sprite(game.world.centerX, 600, "paddle");
+        paddle.anchor.setTo(0.5, 0.5);
+        game.physics.enable(paddle, Phaser.Physics.ARCADE);
+        paddle.body.collideWorldBounds = true;
+        paddle.body.bounce.set(1);
+        paddle.body.immovable = true;
+
+        // Create ball
+        ball = game.add.sprite(game.world.centerX, game.world.centerY, "ball");
+        game.physics.enable(ball, Phaser.Physics.ARCADE);
+        ball.body.velocity.x=100;
+        ball.body.velocity.y=100;
+        ball.checkWorldBounds = true;
+        // ball.body.collideWorldBounds = true;
+        ball.body.collideWorldBounds = true;
+        ball.body.bounce.set(1);
+        ball.anchor.setTo(0.5, 0.5);
+
+        ball.events.onOutOfBounds.add(ballLost, this);
+
+        scoreText = game.add.text(32, 550, 'score: 0', { font: "20px Arial", fill: "#ffffff", align: "left" });
+        livesText = game.add.text(680, 550, 'lives: 3', { font: "20px Arial", fill: "#ffffff", align: "left" });
+        introText = game.add.text(game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
+        introText.anchor.setTo(0.5, 0.5);
+
+        game.input.onDown.add(releaseBall, this);
+
+
+        // Create bricks
+        bricks = game.add.group();
+        bricks.enableBody = true;
+        bricks.physicsBodyType = Phaser.Physics.ARCADE;
+        const ROWS = 4, COLS = 5, OFFSET = 20, BRICK_W = 120, BRICK_H = 35;
+
+        let brick;
+        for (let i = 1; i <= ROWS; i += 1) {
+          for (let j = 0; j <= COLS; j += 1) {
+            const x = j * (BRICK_W + OFFSET);
+            const y = i * (BRICK_H + OFFSET);
+            brick = bricks.create(x, y, "brick");
+            brick.body.bounce.set(1);
+            brick.body.immovable = true;
+          }
+        }
     }
-  };
+
+    function update () {
+      paddle.x = game.input.x;
+
+      if (paddle.x < 24) {
+          paddle.x = 24;
+      } if (paddle.x > game.width - 24) {
+          paddle.x = game.width - 24;
+      }
+
+      if (ballOnPaddle) {
+          ball.body.x = paddle.x;
+      } else {
+          game.physics.arcade.collide(ball, paddle, ballHitPaddle, null, this);
+          game.physics.arcade.collide(ball, bricks, ballHitBrick, null, this);
+      }
+    }
+
+    function releaseBall () {
+      if (ballOnPaddle) {
+          ballOnPaddle = false;
+          ball.body.velocity.y = -300;
+          ball.body.velocity.x = -75;
+          introText.visible = false;
+      }
+    }
+
+  function ballLost () {
+      lives--;
+      livesText.text = 'lives: ' + lives;
+      if (lives === 0) {
+          gameOver();
+      } else {
+          ballOnPaddle = true;
+          ball.reset(paddle.body.x + 16, paddle.y - 16);
+      }
+
+  }
+
+  function gameOver () {
+      ball.body.velocity.setTo(0, 0);
+      introText.text = 'Game Over!';
+      introText.visible = true;
+  }
+
+  function ballHitBrick (_ball, _brick) {
+      _brick.kill();
+
+      score += 10;
+
+      scoreText.text = 'score: ' + score;
+
+      //  Are they any bricks left?
+      if (bricks.countLiving() == 0) {
+          //  New level starts
+          score += 1000;
+          scoreText.text = 'score: ' + score;
+          introText.text = '- Next Level -';
+
+          //  Let's move the ball back to the paddle
+          ballOnPaddle = true;
+          ball.body.velocity.set(0);
+          ball.x = paddle.x + 16;
+          ball.y = paddle.y - 16;
+          ball.animations.stop();
+
+          //  And bring the bricks back from the dead :)
+          bricks.callAll('revive');
+      }
+  }
+
+  function ballHitPaddle (_ball, _paddle) {
+      var diff = 0;
+
+      if (_ball.x < _paddle.x) {
+          //  Ball is on the left-hand side of the paddle
+          diff = _paddle.x - _ball.x;
+          _ball.body.velocity.x = (-10 * diff);
+      } else if (_ball.x > _paddle.x) {
+          //  Ball is on the right-hand side of the paddle
+          diff = _ball.x -_paddle.x;
+          _ball.body.velocity.x = (10 * diff);
+      } else {
+          //  Ball is perfectly in the middle
+          //  Add a little random X to stop it bouncing straight up!
+          _ball.body.velocity.x = 2 + Math.random() * 8;
+      }
+  }
+};
---
+++ game/html/03_paint/paint.js
@@ -0,0 +1,98 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(0, 0, 640, 480);
+
+function putDot(x, y) {
+  console.log(`X: ${x}, Y: ${y}`);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(x, y, 3, 3);
+}
+
+function drawRandom() {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+  const colors = ["red", "blue", "black", "white", "yellow"];
+  const col = colors[getRandomInt(0, colors.length)];
+  ctx.fillStyle = col;
+
+  var times = getRandomInt(2, 150);
+  for (var i = 0; i < times; i++) {
+    const randX = getRandomInt(0, 640)
+    const randY = getRandomInt(0, 480);
+
+    ctx.fillRect(randX, randY, 3, 4);
+  }
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+function relMouseCoords(event) {
+  var totalOffsetX = 0;
+  var totalOffsetY = 0;
+  var canvasX = 0;
+  var canvasY = 0;
+  var currentElement = this;
+
+  do {
+    totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+    totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+  }
+  while (currentElement = currentElement.offsetParent)
+
+  canvasX = event.pageX - totalOffsetX;
+  canvasY = event.pageY - totalOffsetY;
+
+  return {
+    x: canvasX,
+    y: canvasY
+  }
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
+function clearCanvas() {
+  ctx.fillStyle = 'green';
+  ctx.fillRect(0, 0, 640, 480);
+}
+
+function dlCanvas() {
+  let dt = canvas.toDataURL("image/png");
+  /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+  dt = dt.replace(/^data:image\/[^;]*/, "data:application/octet-stream");
+
+  /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+  dt = dt.replace(/^data:application\/octet-stream/, "data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png");
+
+  this.href = dt;
+}
+
+canvas.addEventListener("click", function(ev) {
+  // coords = canvas.relMouseCoords(ev);
+  // alert(`X: ${canvasX}, Y: ${canvasY}`);
+  const coords = getMousePos(canvas, ev);
+
+  putDot(coords.x, coords.y);
+}, false);
+
+document.getElementById("dl").addEventListener("click", dlCanvas, false);
+document.getElementById("reset").addEventListener("click", clearCanvas, false);
+document.getElementById("random").addEventListener("click", drawRandom, false);
