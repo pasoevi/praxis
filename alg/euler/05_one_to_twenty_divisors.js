1bdf7c02e89d2f79f2c5d688cf5aaa961eff543c
Sergi Pasoev <s.pasoev@gmail.com>
2017-09-29 21:13:18 +0400

    smallest number divisible by first 20 natural numbers

---
+++ alg/euler/05_one_to_twenty_divisors.js
@@ -0,0 +1,44 @@
+let divisors = [7, 8, 11, 12, 13, 14, 15, 16]
+console.log(divisors.length);
+let n = 0;
+
+const maxNum = divisors.reduce(function (product, value) {
+  return product * value;
+}, 1);
+
+function divisibleBy(number, factor) {
+  if (number % factor === 0){ 
+    return true;
+  }
+  return false;
+}
+
+function* numGen() {
+  let n = 1;
+  while (true) {
+    if (divisibleBy(n, 20) && divisibleBy(n, 19) && divisibleBy(n, 18) && divisibleBy(n, 17)) {
+      yield n;      
+    }
+    n++;
+  }
+}
+
+let gen = numGen();
+let smallestNum = -1;
+n = gen.next().value;
+
+while (smallestNum < 0) {
+  // Check if n is divisible by all numbers in 'divisors'
+  const goodDivisors = divisors.filter(div => n % div === 0);
+  // console.log(`Good divisors: ${goodDivisors.length}/${divisors.length}`);
+  if (goodDivisors.length > divisors.length / 2) {
+    console.log(`Divisible by: ${goodDivisors}`);
+  }
+
+  if (goodDivisors.length === divisors.length) {
+    smallestNum = n;
+  }
+  n = gen.next().value;
+}
+
+console.log(smallestNum);
