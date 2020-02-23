ac9f759e4ee09bd8bd6d9bdef57892f99adb5c4f
Sergo Pasoevi <spasoevi@gmail.com>
2020-02-24 00:00:01 +0200

    Share files between host and wsl2

--- alg/ascii.py
+++ alg/ascii.py
@@ -1,9 +1,4 @@
-import pprint
 import sys
-import math
-
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
 
 l = int(input())
 h = int(input())
@@ -12,5 +7,35 @@ alphabet = []
 for i in range(h):
     alphabet.append(input())
 
-pprint.pprint(alphabet)
-print("answer")
+""" for c in t.upper():
+    cOrd = ord(c) - ord("A")
+    if cOrd > ord('Z') or cOrd < ord('A'):
+        print(c, file=sys.stderr)
+        print(cOrd, file=sys.stderr)
+
+    for j in range(h):
+        start = cOrd * l
+        stop = start + len(t) * l
+        print(str(start) + " " + str(stop), file=sys.stderr)
+        for i in range(start, stop):
+            if i < stop - 1:
+                print(alphabet[j][i], end = '')
+            else:
+                print(alphabet[j][i])
+
+ """
+
+ for index, c in t.upper():
+    cOrd = ord(c) - ord("A")
+    if cOrd > ord('Z') or cOrd < ord('A'):
+        cord = ord('Z') + 1
+
+    start = cOrd * l
+    stop = start + l
+    for i in range(h):
+        for j in range(start, stop):
+            if index < len(t) - 1:
+                print(alphabet[j][i], end = '')
+            else:
+                print(alphabet[j][i])
+
